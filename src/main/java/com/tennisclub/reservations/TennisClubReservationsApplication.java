d296aa2f7e1e366272a1a6f9082c297e1c280ad9
Rastislav Rosina <xrosina@fi.muni.cz>
2024-12-22 11:50:27 +0100

    feat: base project structure

---
+++ src/main/java/com/tennisclub/reservations/TennisClubReservationsApplication.java
@@ -0,0 +1,13 @@
+package com.tennisclub.reservations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisClubReservationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisClubReservationsApplication.class, args);
+	}
+
+}

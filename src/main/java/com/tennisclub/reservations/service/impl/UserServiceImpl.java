4e3d0d4fa0230e3d5b54425eca733fc353d1929b
Rastislav Rosina <xrosina@fi.muni.cz>
2024-12-29 17:24:20 +0100

    feat: added password hashing to userService

--- src/main/java/com/tennisclub/reservations/service/impl/UserServiceImpl.java
+++ src/main/java/com/tennisclub/reservations/service/impl/UserServiceImpl.java
@@ -8,7 +8,9 @@ import com.tennisclub.reservations.mapper.UserMapper;
 import com.tennisclub.reservations.model.User;
 import com.tennisclub.reservations.repository.UserRepository;
 import com.tennisclub.reservations.service.UserService;
+import com.tennisclub.reservations.util.PasswordUtil;
 import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,28 +19,46 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@Slf4j
 @Transactional
 public class UserServiceImpl extends GenericCrudService<User, UserDto, UserCreateDto, UserDto> implements UserService {
 
     private final UserRepository userRepository;
 
     private final ReservationMapper reservationMapper;
+    private final UserMapper userMapper;
 
     @Autowired
     public UserServiceImpl(UserRepository repository, UserMapper mapper, ReservationMapper reservationMapper) {
         super(repository, mapper, UserDto.class, User.class);
         this.userRepository = repository;
+        this.userMapper = mapper;
         this.reservationMapper = reservationMapper;
     }
 
     @Override
     public UserDto create(UserCreateDto newUser) {
-        return super.create(newUser);
+        log.info("Creating new User: {}", newUser);
+
+        var user = userMapper.toEntityFromSaveDto(newUser);
+        var salt = PasswordUtil.generateSalt();
+        user.setSalt(salt);
+        user.setPassword(PasswordUtil.hashPassword(user.getPassword(), salt));
+
+        return userMapper.toDto(userRepository.save(user));
     }
 
     @Override
     public Optional<UserDto> update(UserDto updateUser) {
-        return super.update(updateUser);
+        log.info("Updating User: {}", updateUser);
+
+        var user = userMapper.toEntityFromUpdateDto(updateUser);
+        user.setPassword(PasswordUtil.hashPassword(user.getPassword(), user.getSalt()));
+
+        if (findById(user.getId()).isPresent())
+            return Optional.ofNullable(userMapper.toDto(userRepository.update(user)));
+
+        return Optional.empty();
     }
 
     @Override

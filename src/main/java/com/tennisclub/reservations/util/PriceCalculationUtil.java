6cc2bb6f8cdd21beff480a5826939c7ac9e79632
Rastislav Rosina <xrosina@fi.muni.cz>
2024-12-30 01:30:48 +0100

    refactor: changed PriceCalculation to util

--- src/main/java/com/tennisclub/reservations/util/PriceCalculationUtil.java
+++ src/main/java/com/tennisclub/reservations/util/PriceCalculationUtil.java
@@ -1,17 +1,14 @@
-package com.tennisclub.reservations.service.impl;
+package com.tennisclub.reservations.util;
 
 import com.tennisclub.reservations.dto.ReservationDto;
 import com.tennisclub.reservations.model.GameType;
-import com.tennisclub.reservations.service.PriceCalculationService;
-import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.Duration;
 
-@Service
-public class PriceCalculationServiceImpl implements PriceCalculationService {
+public class PriceCalculationUtil {
 
-    public BigDecimal calculatePrice(ReservationDto reservation) {
+    public static BigDecimal calculatePrice(ReservationDto reservation) {
         var surfacePrice = reservation.getCourt().getSurface().getMinutePrice();
 
         var duration = Duration.between(reservation.getFrom(), reservation.getTo());
@@ -24,7 +21,7 @@ public class PriceCalculationServiceImpl implements PriceCalculationService {
                 .multiply(multiplier);
     }
 
-    private double getMultiplier(GameType gameType) {
+    private static double getMultiplier(GameType gameType) {
         return switch (gameType) {
             case SINGLES -> 1.0;
             case DOUBLES -> 1.5;
